7640fcba27d6887ba349267c93eabc872810875a
Surfskills <ojjfred@gmail.com>
2023-10-18 13:08:09 +0000

    Sifskills website first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Sufskills cards
+$(document).ready(function() {
+    $(".navbar a, .nav-item a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
